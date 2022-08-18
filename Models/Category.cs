e82f08b826c3aa9411561d96ea9479e08271be7e
Daniel Pinto <apintod@uninorte.edu.co>
2022-08-18 14:46:06 -0500

    DataNotations

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,10 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace projectEF.Models
 {
     public class Category
     {
+        [Key]
         public Guid CategoryId { get; set; }
+        [Required]
+        [MaxLength(150)]
         public string Name { get; set; }
         public string Description { get; set; }
         public virtual ICollection<Task> Tasks { get; set; }
+    
     }
 }
--- Models/Task.cs
+++ Models/Task.cs
@@ -1,13 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace projectEF.Models
 {
     public class Task
     {
+        [Key]
         public Guid TareaId { get; set; }
+        [ForeignKey("CategoryId")]
         public Guid CategoryId { get; set; }
+        [Required]
+        [MaxLength(200)]
         public string Titulo { get; set; }
         public string Description { get; set; }
         public Priority PriorityTask { get; set; }
         public DateTime CreationDate { get; set; }
         public virtual Category Category { get; set; }
+        [NotMapped]
+        public string Resumen { get; set; }
     }
 }
