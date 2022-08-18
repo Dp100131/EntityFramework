98de4001c5f1df19f810bc2d2369bb9af1e1fdc0
Daniel Pinto <apintod@uninorte.edu.co>
2022-08-18 17:22:07 -0500

    Iniciando una migración

--- Models/Category.cs
+++ Models/Category.cs
@@ -10,6 +10,7 @@ namespace projectEF.Models
         //[MaxLength(150)]
         public string Name { get; set; }
         public string Description { get; set; }
+        
         public virtual ICollection<Task> Tasks { get; set; }
     
     }
