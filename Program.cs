d618fe3cd16842744d7e2775e77540de670793fd
Daniel Pinto <apintod@uninorte.edu.co>
2022-08-18 14:58:42 -0500

    Data base create In memory

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using projectEF.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TaskContext>(p => p.UseInMemoryDatabase("TasksDB"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconexion", async ([FromServices] TaskContext dbContext) =>
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
+});
 
 app.Run();
